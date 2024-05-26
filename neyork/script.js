3b219848b9f36387bd7c29d8cffd2fdf93157411
Josiney <101153925+JosineyAlves@users.noreply.github.com>
2024-05-26 13:18:35 -0300

    Add files via upload

--- neyork/script.js
+++ neyork/script.js
@@ -3,10 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const date = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     dateDisplay.innerText = date.toLocaleDateString('en-US', options);
+});
 
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nyt-nav');
-    menuToggle.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var nav = document.getElementById('nyt-nav');
+    nav.classList.toggle('active'); // Alternar a classe 'active'
 });
