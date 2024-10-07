b8f8ffa1dc00eebf894b2636f254689a92477f08
Josiney <101153925+JosineyAlves@users.noreply.github.com>
2024-10-07 13:19:04 -0300

    Add files via upload

--- teste-2/script.js
+++ teste-2/script.js
@@ -1,17 +1,17 @@
 // Função para capturar a cidade a partir do IP usando fetch
 function getCity() {
-    fetch('https://ipapi.co/json/')
-        .then(response => response.json())
-        .then(data => {
-            const cityName = data.city;
-            const cityElements = document.querySelectorAll('.city-name');
-            cityElements.forEach(element => {
-                element.textContent = cityName;
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao obter a cidade:', error);
-        });
+	fetch('https://ipapi.co/json/')
+		.then(response => response.json())
+		.then(data => {
+			const cityName = data.city;
+			const cityElements = document.querySelectorAll('.city-name');
+			cityElements.forEach(element => {
+				element.textContent = cityName;
+			});
+		})
+		.catch(error => {
+			console.error('Erro ao obter a cidade:', error);
+		});
 }
 
 // Executa a função para pegar a cidade quando a página carrega
@@ -26,43 +26,41 @@ const reductionValues = [6, 3, 2];
 let reductionIndex = 0;
 
 function updateStockCount() {
-    if (stockCount > 6) {
-        const nextReduction = reductionValues[reductionIndex];
-        // Verifica se a próxima redução levará o estoque abaixo de 6
-        if (stockCount - nextReduction <= 6) {
-            stockCount = 6;
-        } else {
-            stockCount -= nextReduction;
-        }
+	if (stockCount > 6) {
+		const nextReduction = reductionValues[reductionIndex];
+		// Verifica se a próxima redução levará o estoque abaixo de 6
+		if (stockCount - nextReduction <= 6) {
+			stockCount = 6;
+		} else {
+			stockCount -= nextReduction;
+		}
 
-        stockCountElement.textContent = stockCount;
+		stockCountElement.textContent = stockCount;
 
-        // Atualiza o índice para a próxima redução
-        reductionIndex = (reductionIndex + 1) % reductionValues.length;
-    }
+		// Atualiza o índice para a próxima redução
+		reductionIndex = (reductionIndex + 1) % reductionValues.length;
+	}
 }
 
 function startStockCountdown() {
-    setInterval(updateStockCount, 5000); // Diminui o estoque a cada 5 segundos
+	setInterval(updateStockCount, 3000); // Diminui o estoque a cada 3 segundos
 }
 
 
+document.addEventListener("DOMContentLoaded", function () {
+	var allStarsContainers = document.querySelectorAll('.stars');
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    var allStarsContainers = document.querySelectorAll('.stars');
-
-    allStarsContainers.forEach(function(container) {
-        const rating = parseFloat(container.getAttribute('data-rating'));
-        container.innerHTML = ''; // Clear existing stars if any
-        for (let i = 1; i <= 5; i++) {
-            if (i <= rating) {
-                container.innerHTML += '<span class="star">&#9733;</span>'; // filled star
-            } else if (i > rating && i - 1 < rating) {
-                container.innerHTML += '<span class="star">&#189;</span>'; // half star
-            } else {
-                container.innerHTML += '<span class="star">&#9734;</span>'; // empty star
-            }
-        }
-    });
+	allStarsContainers.forEach(function (container) {
+		const rating = parseFloat(container.getAttribute('data-rating'));
+		container.innerHTML = ''; // Clear existing stars if any
+		for (let i = 1; i <= 5; i++) {
+			if (i <= rating) {
+				container.innerHTML += '<span class="star">★</span>'; // filled star
+			} else if (i > rating && i - 1 < rating) {
+				container.innerHTML += '<span class="star">½</span>'; // half star
+			} else {
+				container.innerHTML += '<span class="star">☆</span>'; // empty star
+			}
+		}
+	});
 });
