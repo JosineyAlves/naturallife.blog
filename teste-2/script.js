37ca631d41e73bd46f5fc022e82479daadad1239
Josiney <101153925+JosineyAlves@users.noreply.github.com>
2024-10-07 14:15:25 -0300

    Add files via upload

--- teste-2/script.js
+++ teste-2/script.js
@@ -1,51 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const elementoContador = document.getElementById('contador');
-    let vagas = 487; // Início com 487 vagas
-    const maxVagas = 500; // Número máximo de vagas
-    let reducoesFeitas = 0; // Controla a quantidade de vezes que a contagem foi reduzida
-    const maxReducoes = Math.floor(Math.random() * 3) + 1; // Número máximo de reduções entre 1 e 3
- 
-    function atualizarContador() {
-       if (vagas < maxVagas) {
-          // Define quando diminuir uma vaga de forma aleatória
-          if (reducoesFeitas < maxReducoes && (Math.random() < 0.1)) { // 10% de chance de reduzir a cada atualização
-             vagas--; // Decrementa vagas
-             reducoesFeitas++; // Incrementa o número de reduções feitas
-          } else if (vagas < maxVagas) {
-             vagas++; // Incrementa vagas
-          }
-          elementoContador.textContent = vagas;
-       }
- 
-       if (vagas < maxVagas) {
-          setTimeout(atualizarContador, Math.random() * 1500 + 2000); // Aumenta o intervalo para dar aspecto de incremento gradual e lento
-       }
-    }
-    atualizarContador(); // Iniciar o processo
- });
- 
- 
 // Função para capturar a cidade a partir do IP usando fetch
 function getCity() {
-    fetch('https://ipapi.co/json/')
-        .then(response => response.json())
-        .then(data => {
-            const cityName = data.city;
-            const cityElements = document.querySelectorAll('.city-name');
-            cityElements.forEach(element => {
-                element.textContent = cityName;
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao obter a cidade:', error);
-        });
+	fetch('https://ipapi.co/json/')
+		.then(response => response.json())
+		.then(data => {
+			const cityName = data.city;
+			const cityElements = document.querySelectorAll('.city-name');
+			cityElements.forEach(element => {
+				element.textContent = cityName;
+			});
+		})
+		.catch(error => {
+			console.error('Erro ao obter a cidade:', error);
+		});
 }
 
 // Executa a função para pegar a cidade quando a página carrega
 getCity();
 
 // Contagem regressiva de estoque
-let stockCount = 139;
+let stockCount = 250;
 const stockCountElement = document.getElementById('stock-count');
 
 // Array de valores de redução
@@ -53,22 +26,41 @@ const reductionValues = [6, 3, 2];
 let reductionIndex = 0;
 
 function updateStockCount() {
-    if (stockCount > 6) {
-        const nextReduction = reductionValues[reductionIndex];
-        // Verifica se a próxima redução levará o estoque abaixo de 6
-        if (stockCount - nextReduction <= 6) {
-            stockCount = 6;
-        } else {
-            stockCount -= nextReduction;
-        }
+	if (stockCount > 19) {
+		const nextReduction = reductionValues[reductionIndex];
+		// Verifica se a próxima redução levará o estoque abaixo de 19
+		if (stockCount - nextReduction <= 19) {
+			stockCount = 19;
+		} else {
+			stockCount -= nextReduction;
+		}
 
-        stockCountElement.textContent = stockCount;
+		stockCountElement.textContent = stockCount;
 
-        // Atualiza o índice para a próxima redução
-        reductionIndex = (reductionIndex + 1) % reductionValues.length;
-    }
+		// Atualiza o índice para a próxima redução
+		reductionIndex = (reductionIndex + 1) % reductionValues.length;
+	}
 }
 
 function startStockCountdown() {
-    setInterval(updateStockCount, 5000); // Diminui o estoque a cada 5 segundos
+	setInterval(updateStockCount, 5000); // Diminui o estoque a cada 3 segundos
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	var allStarsContainers = document.querySelectorAll('.stars');
+
+	allStarsContainers.forEach(function (container) {
+		const rating = parseFloat(container.getAttribute('data-rating'));
+		container.innerHTML = ''; // Clear existing stars if any
+		for (let i = 1; i <= 5; i++) {
+			if (i <= rating) {
+				container.innerHTML += '<span class="star">★</span>'; // filled star
+			} else if (i > rating && i - 1 < rating) {
+				container.innerHTML += '<span class="star">½</span>'; // half star
+			} else {
+				container.innerHTML += '<span class="star">☆</span>'; // empty star
+			}
+		}
+	});
+});
